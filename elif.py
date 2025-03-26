a2c21d03a7ba4456b4969bb47ba584dae6f16879
Himanshuvats <himanshuvats135790@gmail.com>
2025-03-26 14:22:57 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,10 @@
+marks=int(input("enter your number"))
+if(marks >= 90):
+   grade = "a"
+elif(marks >=80 and marks <90):
+    grade = "b"
+elif(marks >=70 and marks <80):
+     grade = "c"
+else:
+    grade = "d"
+print("grade of the student ->", grade)
